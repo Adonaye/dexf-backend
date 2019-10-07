7e1cc6b64e12501f06d94898ea306e3d9094ffed
Adonay Espinosa <adonay.espinosa1@gmail.com>
2019-10-07 16:59:23 -0500

    Included auth routes

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+var authRoutes = require("./routes/auth");
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.use('/', authRoutes);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
