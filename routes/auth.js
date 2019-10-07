7476086131e551d1aba2076c37a1224e286faf8e
Adonay Espinosa <adonay.espinosa1@gmail.com>
2019-10-07 16:57:47 -0500

    Twitter oauth request_token

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const request = require('request');
+const qs = require('querystring');
+const router = express.Router();
+
+router.get('/oauth_request', function(req, res) {
+    const oauth = {
+        callback: "https://127.0.0.1:3000/callback",
+        consumer_key: "T4MrAAp7iN2qfXvgl0mB2vNvd",
+        consumer_secret: "UgaqFz4Lgad7t3KlEEjy2Mn2wJslDBVljHNGIYgkvTXVnccgOs"
+    }
+    const url = "https://api.twitter.com/oauth/request_token";
+    request.post({
+        url: url,
+        oauth: oauth
+    }, function(err, httpResponse, body) {
+        res.status(httpResponse.statusCode).send(qs.parse(body));
+    });
+});
+
+module.exports = router;
