fcf2c609ef0c261e3ada287b4e96eac8f89cdd44
Adonay Espinosa <adonay.espinosa1@gmail.com>
2019-10-07 16:58:02 -0500

    Added empty auth_controller

---
+++ controllers/auth_controller.js
@@ -0,0 +1,3 @@
+const CONSUMER_KEY = "T4MrAAp7iN2qfXvgl0mB2vNvd",
+      CONSUMER_SECRET = "UgaqFz4Lgad7t3KlEEjy2Mn2wJslDBVljHNGIYgkvTXVnccgOs",
+      AUTH_CALLBACK_URL = "https://127.0.0.1:3000/callback"
