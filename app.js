1ddd96f49d597663519b7bbb5a92bba6ae01d56e
Adonay Espinosa <adonay.espinosa1@gmail.com>
2019-10-08 08:00:28 -0500

    Joined user routes

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 
 var authRoutes = require("./routes/auth");
+var userRoutes = require("./routes/user");
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.use('/', authRoutes);
+app.use('/', userRoutes);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
