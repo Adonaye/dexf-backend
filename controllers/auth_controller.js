3628c51d20159288eb2d03423b711c1b80cecac9
Adonay Espinosa <adonay.espinosa1@gmail.com>
2019-10-08 07:41:07 -0500

    Added getOauthParams method

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -2,3 +2,11 @@ const CONSUMER_KEY = "T4MrAAp7iN2qfXvgl0mB2vNvd", // envvar
       CONSUMER_SECRET = "UgaqFz4Lgad7t3KlEEjy2Mn2wJslDBVljHNGIYgkvTXVnccgOs", //envvar
       DEFAULT_AUTH_CALLBACK_URL = "https://127.0.0.1:3000/callback";
     
+function getOauthParams(req) {
+    let requestedCallbackUrl = req.body.callbackUrl;
+    return {
+        callback: requestedCallbackUrl || DEFAULT_AUTH_CALLBACK_URL,
+        consumer_key: CONSUMER_KEY,
+        consumer_secret: CONSUMER_SECRET
+    }
+}
