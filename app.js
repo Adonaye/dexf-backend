d7935e462bfd2972585cdfef6a69768449ac5d23
Adonay Espinosa <adonay.espinosa1@gmail.com>
2019-10-08 15:05:21 -0500

    Functional fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 require('dotenv').config();
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
+app.use(cors());
 
 
 app.get('/', function (req, res) {
