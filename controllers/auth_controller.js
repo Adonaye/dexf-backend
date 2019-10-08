755e6adaed418140a5a30501b96efed20976ecb5
Adonay Espinosa <adonay.espinosa1@gmail.com>
2019-10-08 10:27:40 -0500

    Changed URL for no secure http

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,6 +1,6 @@
 const CONSUMER_KEY = "T4MrAAp7iN2qfXvgl0mB2vNvd", // envvar
       CONSUMER_SECRET = "UgaqFz4Lgad7t3KlEEjy2Mn2wJslDBVljHNGIYgkvTXVnccgOs", //envvar
-      DEFAULT_AUTH_CALLBACK_URL = "https://127.0.0.1:3000/callback";
+      DEFAULT_AUTH_CALLBACK_URL = "http://127.0.0.1:3000/callback";
     
 function getOauthParams(params = {}) {
     let requestedCallbackUrl = params.callbackUrl;
